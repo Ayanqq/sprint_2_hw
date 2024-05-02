ffb793d889ca73836df70740a3af055b7f584b18
Ayanqq <nazbievayan16@gmail.com>
2024-05-02 21:55:57 +0500

    hw 5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -19,17 +19,13 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
                 <Route path={'/'} element={<PreJunior/>}/>
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path={'/'} element={<PreJunior/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
 
-                <Route path={'/pre-junior'} element={<PreJunior/>}/>
-                <Route path={'/junior'} element={<Junior/>}/>
-                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
                 {/*<Route ...*/}
                 {/*<Route ...*/}
